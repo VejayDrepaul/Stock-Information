f1d2a7366af7da6d7f51d096f81202d95d61c09c
Vejay Drepaul <vejayddrepaul@gmail.com>
2022-03-20 00:11:46 -0400

    Added more chart options

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import os
 import streamlit as st
 import pandas as pd
-from enum import auto
-from multiprocessing.connection import wait
 from dotenv import load_dotenv
 load_dotenv()
 ALPHA_VANTAGE_KEY = os.getenv("ALPHA_VANTAGE_KEY")
@@ -15,9 +13,11 @@ st.markdown("<h1 style='text-align: center'>Stock Information</h1>", unsafe_allo
 stock_ticker = st.text_input("Ticker Symbol", placeholder="Enter Ticker Symbol: ").upper()
 
 # Checkboxes & checkboxes container
-csv_checkbox_col, chart_checkbox_col = st.columns(2)
-view_csv = csv_checkbox_col.checkbox("Show CSV File")
-view_chart = chart_checkbox_col.checkbox("Show Stock Chart")
+csv_col, daily_col, intraday_col, monthly_col = st.columns(4)
+view_csv = csv_col.checkbox("Show CSV File", help="Selecting this option will show the stock's csv file")
+view_chart = daily_col.checkbox("Daily Stock Chart", help="Selecting this option will show the stock's daily chart")
+view_intraday = intraday_col.checkbox("Intraday Chart", help="Selecting this option will show the stock's intraday chart")
+view_monthly = monthly_col.checkbox("Monthly Chart", help="Selecting this option will show the stock's montly chart")
 
 #csv_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={stock_ticker}&apikey={ALPHA_VANTAGE_KEY}&datatype=csv"
 df = pd.read_csv("daily_IBM.csv")
@@ -32,7 +32,7 @@ if view_csv == False and view_chart == False:
 elif view_csv == True and view_chart == True:
     placeholder.write("To many paramaters selected")
 elif view_csv == True and view_chart == False:
-    placeholder.write(df)
+    placeholder.dataframe(df)
 elif view_csv == False and view_chart == True:
     placeholder.line_chart(chart_df, width=1162, use_container_width=False)
 else:
